3b6438f6f0bb2b747982d1ace6f632a7136e7a52
Zeyad Ahmed <zeyadfahem1@gmail.com>
2024-11-08 19:33:01 +0200

    fix fetching wrong env variable for roles endpoints

--- config/initializers/01_dotenv.rb
+++ config/initializers/01_dotenv.rb
@@ -11,7 +11,7 @@ Rails.application.config.auth = {
   client_secret: ENV['KC_CLIENT_SECRET'],
   token_endpoint: ENV['AUTH_TOKEN_ENDPOINT'],
   users_endpoint: ENV['MANAGE_USERS_ENDPOINT'],
-  roles_enpoint: ENV['MANAGE_ROLES_ENDPOINT'],
+  roles_endpoint: ENV['MANAGE_ROLES_ENDPOINT'],
   admin_role: ENV['ADMIN_ROLE'],
   user_role: ENV['USER_ROLE']
 }
