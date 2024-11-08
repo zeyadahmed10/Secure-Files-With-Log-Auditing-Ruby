034f53018a399c58f889feac1fa76ed3047e4030
Zeyad Ahmed <zeyadfahem1@gmail.com>
2024-11-08 19:33:22 +0200

    add signin and signup under auth resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
   namespace :api do
     namespace :v1 do
-      resources :files , only: [:index, :create, :show] do
+      resources :files, only: [:index, :create, :show]
+      resources :auth, only: [] do
+        collection do
+          post 'signin', to: 'auth#signin'
+          post 'signup', to: 'auth#signup'
+        end
       end
-      post 'signin', to: '#signin'
-      post 'signup', to: '#signup'
     end
   end
 end
