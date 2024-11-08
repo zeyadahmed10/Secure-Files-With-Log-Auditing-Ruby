2e740247747618d443c52cc13a83daea7402c15f
Zeyad Ahmed <zeyadfahem1@gmail.com>
2024-11-08 14:34:46 +0200

    adding 01_dotenv.rb to load env variables from .env file for auth

---
+++ config/initializers/01_dotenv.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+if Rails.env.development? || Rails.env.test?
+    require 'dotenv'
+    Dotenv.load
+end
+
+Rails.application.config.auth = {
+  domain: ENV['KC_DOMAIN'],
+  realm: ENV['KC_REALM'],
+  client_id: ENV['KC_CLIENT'],
+  client_secret: ENV['KC_CLIENT_SECRET'],
+  token_endpoint: ENV['AUTH_TOKEN_ENDPOINT'],
+  users_endpoint: ENV['MANAGE_USERS_ENDPOINT'],
+  roles_enpoint: ENV['MANAGE_ROLES_ENDPOINT'],
+  admin_role: ENV['ADMIN_ROLE'],
+  user_role: ENV['USER_ROLE']
+}
